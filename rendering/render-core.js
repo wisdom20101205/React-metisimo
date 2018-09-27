157a313039e0261d0e33ad182122da60c4d3a393
Tiger <wisdom20101205@outlook.com>
2018-09-26 19:42:53 -0700

    Readme file

--- rendering/render-core.js
+++ rendering/render-core.js
@@ -7,14 +7,14 @@ var viewNameRegex = /(([-_a-z0-9]+)(\\|\/))?([-_a-z0-9]+)\.(jsx|js|html)$/i;
 var baseTemplatePath = __dirname + '/template.html';
 var baseTemplate = fs.readFileSync(baseTemplatePath,"utf-8");
 
-fs.watchFile(baseTemplatePath,function(curr, prev){
+fs.watchFile(baseTemplatePath,function(curr, prev) {
 	baseTemplate = fs.readFileSync(baseTemplatePath,"utf-8");
 });
 
 export default {
-	parse(entryPoint, content,  model){
+	parse(entryPoint, content,  model) {
 		var markup = mustache.render(baseTemplate,{
-			content, 
+			content,
 			entryPoint,
 			model : JSON.stringify(model)
 		});
